0f9ef68650bb3351ebc1e70950fb66ed2d905b24
Stephen Fluin <stephen.fluin@gmail.com>
2017-12-03 16:27:33 -0800

    Add additional detectors + refactor

--- generate-http2-push.js
+++ generate-http2-push.js
@@ -2,71 +2,65 @@
 fs = require('fs');
 
 fs.readdir('dist/', (err, files) => {
-    if(!files) {
-        console.log("No dist folder found to write to.");
+    if (!files) {
+        console.log('No dist folder found to write to.');
         return;
     }
+    const indexMatches = [
+        '.gz',
+        '.map',
+        '3rdpartylicenses.txt',
+        'ngsw-manifest.json',
+        'ngsw.json',
+        'worker-basic.min.js',
+        'ngsw-worker.js',
+        'favicon',
+        'index.html',
+    ];
     files = files.filter(file => {
-        if(file.indexOf('.gz') != -1) {
+        for (const path of indexMatches) {
+            if (file.indexOf(path) != -1) {
+                return false;
+            }
+        }
+        if (fs.lstatSync(`dist/${file}`).isDirectory()) {
             return false;
         }
-        if(file.indexOf('.map') != -1) {
-            return false;
-        }
-        if(file.indexOf('3rdpartylicenses.txt') != -1) {
-            return false;
-        }
-        if(file.indexOf('ngsw-manifest.json') != -1) {
-            return false;
-        }
-        if(file.indexOf('worker-basic.min.js') != -1) {
-            return false;
-        }
-        if(file.indexOf('favicon') != -1) {
-            return false
-        }
-        if(file.indexOf('index.html') != -1) {
-            return false;
-        }
-        if(fs.lstatSync(`dist/${file}`).isDirectory()) {
-            return false;
-        }
-        if(file.indexOf('.bundle.') != -1 || file.indexOf('.chunk.')) {
+        if (file.indexOf('.bundle.') != -1 || file.indexOf('.chunk.')) {
             return true;
         } else {
             return false;
         }
-    })
+    });
     let result = '';
-    for(let file of files) {
+    for (let file of files) {
         let type;
-        if(file.substr(-3) == "css") {
+        if (file.substr(-3) == 'css') {
             type = 'style';
         } else {
             type = 'script';
         }
-        result += `</${file}>;rel=preload;as=${type},`
+        result += `</${file}>;rel=preload;as=${type},`;
     }
     updateWith(result);
-})
+});
 
 function updateWith(result) {
-    fs.readFile('firebase.json', 'utf8', function(err,data) {
-        if(err) {
+    fs.readFile('firebase.json', 'utf8', function(err, data) {
+        if (err) {
             return console.log(err);
         }
         let re = /("headers":\s*\[\s*{\s*"key":\s*"Link",\s*"value":\s*")(.*)("\s*}\s*\])/gm;
 
-        if(re.exec(data)) {
-            let newConfig = data.replace(re , `$1${result}$3`);
+        if (re.exec(data)) {
+            let newConfig = data.replace(re, `$1${result}$3`);
             fs.writeFile('firebase.json', newConfig, 'utf8', function(err) {
                 if (err) return console.log(err);
-                console.log("firebase.json updated successfully.");
-            })
+                console.log('firebase.json updated successfully.');
+            });
         } else {
             console.log("Couldn't find a valid firebase config to update.");
             return;
         }
-        
     });
 }
